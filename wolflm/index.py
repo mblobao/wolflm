54f38eadb127982fbe79782ba65b3e85f378c324
Marcel Britto Lobão <marcelblobao@gmail.com>
2026-05-07 13:42:59 -0300

    verify

--- wolflm/index.py
+++ wolflm/index.py
@@ -7,12 +7,15 @@ load_dotenv(override=True)
 VIEW_PATH = Path(__file__).parent / 'view'
 
 
-page_list = [
-    st.Page(VIEW_PATH / 'chat.py', title='Chat', default=True),
-]
+# page_list = [
+#     st.Page(VIEW_PATH / 'chat.py', title='Chat', default=True),
+# ]
 
-def get_page(title: str = None):
-    return next(filter(lambda x: x.title == title, page_list))
+# def get_page(title: str = None):
+#     return next(filter(lambda x: x.title == title, page_list))
 
-pages = st.navigation(page_list, position='top',)
-pages.run()
+# pages = st.navigation(page_list, position='top',)
+# pages.run()
+
+st.write(VIEW_PATH)
+st.write(VIEW_PATH.exists())
