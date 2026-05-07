b92143f90052fd900e2fb01b64d838abdd57157a
Marcel Britto Lobão <marcelblobao@gmail.com>
2026-05-07 13:45:40 -0300

    new

--- wolflm/index.py
+++ wolflm/index.py
@@ -7,7 +7,7 @@ load_dotenv(override=True)
 VIEW_PATH = Path(__file__).parent / 'view'
 
 page_list = [
-    st.Page(VIEW_PATH / 'chat.py', title='Chat', default=True),
+    st.Page(str(VIEW_PATH / 'chat.py'), title='Chat', default=True),
 ]
 
 def get_page(title: str = None):
